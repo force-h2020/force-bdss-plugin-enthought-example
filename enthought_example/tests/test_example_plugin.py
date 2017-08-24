9bc3c50e610e8a7d05cb007f93c7066a91cdb858
Stefano Borini <sborini@enthought.com>
2017-08-24 15:25:53 +0100

    Coverage

---
+++ enthought_example/tests/test_example_plugin.py
@@ -0,0 +1,13 @@
+import unittest
+
+from enthought_example.example_plugin import ExamplePlugin
+
+
+class TestExamplePlugin(unittest.TestCase):
+    def test_basic_functionality(self):
+        plugin = ExamplePlugin()
+        self.assertEqual(len(plugin.data_source_factories), 1)
+        self.assertEqual(len(plugin.kpi_calculator_factories), 1)
+        self.assertEqual(len(plugin.mco_factories), 1)
+        self.assertEqual(len(plugin.notification_listener_factories), 1)
+        self.assertEqual(len(plugin.ui_hooks_factories), 1)
