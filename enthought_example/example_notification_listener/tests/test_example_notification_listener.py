0a1684775260f66d0ff2ef1dfeff991a2b64801a
Stefano Borini <sborini@enthought.com>
2017-08-24 11:31:31 +0100

    Fixed tests for notification listener

---
+++ enthought_example/example_notification_listener/tests/test_example_notification_listener.py
@@ -0,0 +1,61 @@
+import unittest
+import sys
+from contextlib import contextmanager
+from six import StringIO
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+from force_bdss.api import MCOStartEvent, MCOProgressEvent, MCOFinishEvent
+from force_bdss.tests.utils import captured_output
+
+
+from enthought_example.example_notification_listener\
+    .example_notification_listener_model import (
+        ExampleNotificationListenerModel)
+from enthought_example.example_notification_listener\
+    .example_notification_listener_factory import (
+        ExampleNotificationListenerFactory)
+from enthought_example.example_notification_listener\
+    .example_notification_listener import (
+        ExampleNotificationListener)
+
+
+class TestExampleNotificationListener(unittest.TestCase):
+    def test_initialization(self):
+        listener = ExampleNotificationListener(
+            mock.Mock(spec=ExampleNotificationListenerFactory))
+        model = mock.Mock(spec=ExampleNotificationListenerModel)
+        with captured_output() as (out, err):
+            listener.initialize(model)
+            listener.deliver(MCOStartEvent(
+                input_names=("foo", "bar"),
+                output_names=("baz", "quux")))
+            listener.deliver(MCOProgressEvent(
+                input=(1.0, 2.0),
+                output=(3.0, 4.0)
+            ))
+            listener.deliver(MCOFinishEvent())
+            listener.finalize()
+
+        self.assertEqual(
+            out.getvalue(),
+            "Initializing\n"
+            "MCOStartEvent ('foo', 'bar') ('baz', 'quux')\n"
+            "MCOProgressEvent (1.0, 2.0) (3.0, 4.0)\n"
+            "MCOFinishEvent\n"
+            "Finalizing\n"
+        )
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
---
+++ enthought_example/example_notification_listener/tests/test_example_notification_listener_model.py
@@ -0,0 +1,21 @@
+import unittest
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+from enthought_example.example_notification_listener\
+     .example_notification_listener_factory import (
+            ExampleNotificationListenerFactory)
+
+from enthought_example.example_notification_listener\
+    .example_notification_listener_model import (
+        ExampleNotificationListenerModel)
+
+
+class TestExampleNotificationListenerModel(unittest.TestCase):
+    def test_initialization(self):
+        factory = mock.Mock(spec=ExampleNotificationListenerFactory)
+        model = ExampleNotificationListenerModel(factory)
+        self.assertEqual(model.factory, factory)
