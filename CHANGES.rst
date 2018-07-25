e6d91a9414ba8a9d3d74d0b170df9ec6d3ba7053
Stefano Borini <sborini@enthought.com>
2018-07-25 12:28:24 +0100

    Release branch for 0.2.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,9 @@
 Changelog 
 ---------
 
+Version 0.2.0
+-------------
+
 Version 0.1.0
 -------------
 - Initial implementation
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.2.0.dev0"
+VERSION = "0.2.0"
 
 
 # Read description
