4435fe55906af637b288dd17045d06818b1b5a47
Matthew Evans <me388@cam.ac.uk>
2019-03-12 17:08:15 +0000

    Made RangedMCOParameter use traits Range
    - Requires a custom init to ensure that boundaries are set before
    initial value is.

--- enthought_example/example_mco/parameters.py
+++ enthought_example/example_mco/parameters.py
@@ -1,20 +1,41 @@
-from traits.api import Float
+from traits.api import Float, Range
+from traitsui.api import View, Item
 
-from force_bdss.api import (
-    BaseMCOParameter,
-    BaseMCOParameterFactory)
+from force_bdss.api import BaseMCOParameter, BaseMCOParameterFactory
 
 
 class RangedMCOParameter(BaseMCOParameter):
-    """Expresses a MCO parameter that has a range between two floating
-    point values."""
-    initial_value = Float(0.0)
-    lower_bound = Float(0.0)
-    upper_bound = Float(1.0)
+    """ Expresses a MCO parameter that has a range between two floating
+    point values.
+
+    """
+
+    lower_bound = Float()
+    upper_bound = Float()
+    initial_value = Range(
+        low="lower_bound",
+        high="upper_bound",
+        label="Initial value",
+        depends_on="lower_bound,upper_bound",
+    )
+
+    traits_view = View(Item("lower_bound"), Item("upper_bound"), Item("initial_value"))
+
+    def __init__(self, *args, **model_data):
+        """ Custom init to prevent argument ordering issues after
+        expansion; since the validity of `intial_value` is checked when
+        it is set, the bounds need to be set first.
+
+        """
+        cached_initial_value = model_data.pop("initial_value", None)
+        super(RangedMCOParameter, self).__init__(*args, **model_data)
+        if cached_initial_value:
+            self.initial_value = cached_initial_value
 
 
 class RangedMCOParameterFactory(BaseMCOParameterFactory):
     """The factory of the above model"""
+
     #: This identifier must be unique for your parameter.
     #: Once again you are fully responsible for its uniqueness within the scope
     #: of the MCO it belongs to. You can have the same identifier if and only
