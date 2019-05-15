e6d923c51f08835ccbc4ec3f2479cd31e7712e5e
Matthew Evans <me388@cam.ac.uk>
2019-05-15 17:24:25 +0100

    Incremented factory number after rebase

--- eggbox_potential_sampler/tests/test_eggbox_plugin.py
+++ eggbox_potential_sampler/tests/test_eggbox_plugin.py
@@ -7,6 +7,6 @@ class TestExamplePlugin(unittest.TestCase):
     def test_basic_functionality(self):
         plugin = EggboxPlugin()
         self.assertEqual(len(plugin.data_source_factories), 1)
-        self.assertEqual(len(plugin.mco_factories), 1)
+        self.assertEqual(len(plugin.mco_factories), 2)
         self.assertEqual(len(plugin.notification_listener_factories), 0)
         self.assertEqual(len(plugin.ui_hooks_factories), 0)
