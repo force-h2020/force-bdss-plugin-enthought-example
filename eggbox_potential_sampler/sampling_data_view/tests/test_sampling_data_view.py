ee36ca9969877731aa65885dbfbe0623dbfc4f65
Nicola De Mitri <ndemitri@enthought.com>
2019-07-25 11:46:46 +0100

    Make test independent of traitsui.view.View's repr

--- eggbox_potential_sampler/sampling_data_view/tests/test_sampling_data_view.py
+++ eggbox_potential_sampler/sampling_data_view/tests/test_sampling_data_view.py
@@ -1,9 +1,9 @@
-import textwrap
 import unittest
 
 from chaco.lineplot import LinePlot
 
 from force_wfmanager.model.analysis_model import AnalysisModel
+from force_wfmanager.ui.ui_utils import model_info
 
 from eggbox_potential_sampler.sampling_data_view import (
     SamplingDataView)
@@ -16,24 +16,9 @@ class TestSamplingDataView(unittest.TestCase):
         self.data_view = SamplingDataView(analysis_model=self.analysis_model)
 
     def test_data_view(self):
-        # check the view composition (there are probably better ways of
-        # checking a traitsui.view.View)
-        expected_view = textwrap.dedent("""\
-            ( HGroup(
-                Item( 'colormap_plot'
-                      object = 'object',
-                      style  = 'custom'
-                ),
-                Item( 'sampling_plot'
-                      object = 'object',
-                      style  = 'custom'
-                ),
-                object = 'object',
-                style  = 'simple'
-            ) )""")
+        # check the view composition
         self.assertEqual(
-            repr(self.data_view.trait_view_elements().content["traits_view"]),
-            expected_view
+            model_info(self.data_view), ['colormap_plot', 'sampling_plot']
         )
 
         self.assertEqual(
