c801eb33ca6c90e5fffeadc41ed604abcee57d2c
sparsonslab <sparsonslab@gmail.com>
2020-05-28 15:32:30 +0100

    merge circ-wave tests

--- troughs_and_waves/circular_wave/tests/test_circular_wave.py
+++ troughs_and_waves/circular_wave/tests/test_circular_wave.py
@@ -4,50 +4,52 @@
 
 import unittest
 
-from unittest import mock
-
 from traits.testing.api import UnittestTools
-from force_bdss.api import DataValue, Slot, BaseDataSourceFactory
+from force_bdss.api import DataValue, Slot
+
+from troughs_and_waves.troughs_and_waves_plugin import TroughsAndWavesPlugin
 
 from troughs_and_waves.circular_wave.circular_wave import CircularWave
 from troughs_and_waves.circular_wave.circular_wave_model import (
     CircularWaveModel
 )
+from troughs_and_waves.circular_wave.circular_wave_factory import (
+    CircularWaveFactory
+)
 
 
 class TestGaussian(unittest.TestCase, UnittestTools):
     def setUp(self):
-        self.factory = mock.Mock(spec=BaseDataSourceFactory)
+        self.plugin = TroughsAndWavesPlugin()
+        self.factory = CircularWaveFactory(plugin=self.plugin)
+        self.ds = self.factory.create_data_source()
+        self.model = self.factory.create_model()
 
     def test_initialization(self):
-        ds = CircularWave(self.factory)
-        self.assertEqual(ds.factory, self.factory)
+        self.assertNotEqual(self.factory.id, "")
+        self.assertEqual(self.factory.plugin_id, self.plugin.id)
+        self.assertIsInstance(self.ds, CircularWave)
+        self.assertIsInstance(self.model, CircularWaveModel)
+
+    def test_slots(self):
+        slots = self.ds.slots(self.model)
+        self.assertEqual(len(slots), 2)
+        self.assertEqual(len(slots[0]), 2)
+        self.assertEqual(len(slots[1]), 1)
+        self.assertIsInstance(slots[0][0], Slot)
+        self.assertIsInstance(slots[1][0], Slot)
 
     def test_run(self):
-        ds = CircularWave(self.factory)
-        model = CircularWaveModel(self.factory)
 
-        model.peak = 2.0
-        model.cent_x = 0.0
-        model.cent_y = 0.0
-        model.wavelength = 1.0
+        self.model.peak = 2.0
+        self.model.cent_x = 0.0
+        self.model.cent_y = 0.0
+        self.model.wavelength = 1.0
 
         mock_params = [
             DataValue(value=0.5),
             DataValue(value=0.0),
         ]
 
-        result = ds.run(model, mock_params)
-
+        result = self.ds.run(self.model, mock_params)
         self.assertAlmostEqual(result[0].value, -2.0)
-
-    def test_slots(self):
-        ds = CircularWave(self.factory)
-        model = CircularWaveModel(self.factory)
-        slots = ds.slots(model)
-
-        self.assertEqual(len(slots), 2)
-        self.assertEqual(len(slots[0]), 2)
-        self.assertEqual(len(slots[1]), 1)
-        self.assertIsInstance(slots[0][0], Slot)
-        self.assertIsInstance(slots[1][0], Slot)
